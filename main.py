d773c4fb032e042db0af123b420c08aa341ce692
imiika <132232349+imiika@users.noreply.github.com>
2023-10-19 14:29:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def show_dicts1(dict):
 
 def show_dicts(dict):
     dict_df = pd.DataFrame( dict, index = [key for key, _ in dicts[0].items()], columns = [ 'Word', 'Frequency' ] )
-    st.dataframe( dict_df )
+    st.markdown( dict_df.style.hide( axis="index" ).to_html(), unsafe_allow_html = True )
 
 if choose == "Home" :
     st.title( "Reconnaissance des mots arabes manuscrits pris de la base de données IFN/ENIT" )
