721be0cb30dc066c381309ec0f55a16934a9b69d
imiika <132232349+imiika@users.noreply.github.com>
2023-10-19 12:42:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,9 @@ with st.sidebar :
     )
 
 def read_data():
-    doc_names = os.listdir( 'Documents/' ).reverse()
+    doc_names = os.listdir( 'Documents/' )
+    st.write(doc_names)
+    doc_names.reverse()
     st.write(doc_names)
     docs = []
     for doc in doc_names :
