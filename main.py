875a3b0a5b904598e720f05793f9edfc22da24db
imiika <132232349+imiika@users.noreply.github.com>
2023-10-19 14:58:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def show_dicts1(dict):
 def show_dicts(dict):
     dict_df = pd.DataFrame.from_dict( dict, orient = 'index', columns = ['Frequency'] )
     dict_df.index.name = 'Word'
-    st.table( data = dict_df, width = 1000 )
+    st.table( dict_df )
     
     #st.markdown( dict_df.style.hide( axis="index" ).to_html(), unsafe_allow_html = True )
 
