1b6e56af59e36bde0ad7816a025523b5e86b4b98
imiika <132232349+imiika@users.noreply.github.com>
2023-10-19 10:12:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import cv2
+import os
+import matplotlib.pyplot as plt
+from streamlit_option_menu import option_menu
 
+with st.sidebar :
+    choose = option_menu("Menu", ["Home", "Dictionaries", "TF-IDF", "Contact", "Help"],
+                         icons=['house', 'gear-wide', 'table', 'stars','person lines fill'],
+                         menu_icon="menu-app",
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
