02440eae744a845ba7c7b327284ec00972bc18d7
imiika <132232349+imiika@users.noreply.github.com>
2023-10-19 17:42:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ elif choose == "Dictionary per document" :
     for i in range( len(docs) ) :
         st.write( "## Text n", i+1 )
         st.write( "### Original text :" )
-        st.write( "######", docs[i] )
+        st.write( "#####", docs[i] )
         
         st.write( "### Text after cleaning and preprocessing : lower case, stopwords and non-words removal, stemming :" )
         st.write( preprocessed_docs[i] )
