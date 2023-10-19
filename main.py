123b4b2d18f31edea8915f07b3b7007d5d90104f
imiika <132232349+imiika@users.noreply.github.com>
2023-10-19 17:45:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -111,17 +111,14 @@ elif choose == "Dictionary per document" :
     for i in range( len(docs) ) :
         st.write( "## Text n", i+1 )
         st.write( "### Original text :" )
-        st.write( "######", docs[i] )
         st.write( "#####", docs[i] )
-        st.write( "####", docs[i] )
-        st.write( docs[i] )
         
         st.write( "### Text after cleaning and preprocessing : lower case, stopwords and non-words removal, stemming :" )
         st.write( preprocessed_docs[i] )
 
         dicts = create_dicts( preprocessed_docs )
         st.write( "### Dictionary :" )
-        show_dicts( dicts[i] )
+        show_dicts( "#####", dicts[i] )
 
 elif choose == "TF-IDF" :
     docs = read_data()
