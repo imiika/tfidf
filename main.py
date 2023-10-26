e8439e5f3036ec94080e9b2e0666f4287f4ddfc5
imiika <132232349+imiika@users.noreply.github.com>
2023-10-26 16:12:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ nltk.download('words')
 
 with st.sidebar :
     choose = option_menu("Menu", ["Home", "Dictionary per document", "Descriptors", "TF-IDF", "Contact", "Help"],
-                         icons=['house', 'table 2 columns', 'search', 'table', 'stars','person lines fill'],
+                         icons=['house', 'table 2 columns', 'search', 'file-earmark-binary', 'stars','person lines fill'],
                          menu_icon="menu-app",
                          styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
